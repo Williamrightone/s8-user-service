bdcbab7b64dcf04d551b87d3f2cb51422c2d5159
william <willy4543@gmail.com>
2023-11-25 15:22:57 +0800

    init-project

---
+++ src/test/java/cc/synpulse8/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.synpulse8.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
